0022d63871590a6d2dcda24da0862eec8bfed1fa
kszorin <kszorin@yandex.ru>
2018-02-26 22:44:50 +0700

    added main methods in World-class

--- app/src/main/java/ru/kszorin/seaworldkotlin/models/Creature.kt
+++ app/src/main/java/ru/kszorin/seaworldkotlin/models/Creature.kt
@@ -6,15 +6,28 @@ import ru.kszorin.seaworldkotlin.R
 /**
  * Created on 23.02.2018.
  */
-abstract class Creature(id : Int, pos : Pair<Int, Int>) {
+abstract class Creature(val id: Int, var pos: Pair<Int, Int>) : Comparable<Creature> {
 
-    abstract val species : Species
-    abstract val environs : Byte
-
-
-    companion object {
-        enum class Species(bmpId : Int) {ORCA(R.drawable.orca), PENGUIN(R.drawable.tux)}
-    }
+    abstract val species: Species
+    abstract val environs: Byte
+    var isAlive = true
 
     abstract fun lifeStep()
+
+    override fun compareTo(other: Creature): Int {
+        return if (this.pos.second != other.pos.second) {
+            this.pos.second - other.pos.second
+        } else {
+            this.pos.first - other.pos.first
+        }
+    }
+
+    companion object {
+        enum class Species(val bmpId: Int) {
+            ORCA(R.drawable.orca),
+            PENGUIN(R.drawable.tux);
+        }
+    }
+
+
 }
--- app/src/main/java/ru/kszorin/seaworldkotlin/models/World.kt
+++ app/src/main/java/ru/kszorin/seaworldkotlin/models/World.kt
@@ -1,21 +1,70 @@
 package ru.kszorin.seaworldkotlin.models
 
+import android.util.Log
+import java.util.*
+
 /**
  * Created on 24.02.2018.
  */
 class World {
+    private var creaturesNumber: MutableMap<Creature.Companion.Species, Int> = HashMap()
+    private var creaturesIdCounter = 0
+    private var waterSpace = array2dOfInt(FIELD_SIZE_Y, FIELD_SIZE_X)
+    private var creaturesMap: MutableMap<Int, Creature> = TreeMap()
+
+    fun reset() {
+        creaturesNumber.put(Creature.Companion.Species.ORCA, FIELD_SIZE_X * FIELD_SIZE_Y * ORCAS_PERCENT_FILLING / 100)
+        creaturesNumber.put(Creature.Companion.Species.PENGUIN, FIELD_SIZE_X * FIELD_SIZE_Y * PENGUINS_PERCENT_FILLING / 100)
+        for (i in waterSpace.indices) {
+            for (j in waterSpace[i].indices) {
+                waterSpace[i][j] = FREE_WATER_CODE
+            }
+        }
+        creaturesIdCounter = 0
+        creaturesMap.clear()
+
+        // create creatures and put them on the field
+        for (species in creaturesNumber.keys) {
+            for (i in 0 until creaturesNumber[species]!!) {
+                val pos = findFreePosition()
+                creaturesMap.put(creaturesIdCounter, when (species) {
+                    Creature.Companion.Species.ORCA -> Orca(creaturesIdCounter, pos)
+                    Creature.Companion.Species.PENGUIN -> Pinguin(creaturesIdCounter, pos)
+                })
+                Log.d(TAG, "${species.name} (id = $creaturesIdCounter) was created and put in [${pos.first}, ${pos.second}]")
+                creaturesIdCounter++;
+            }
+        }
+    }
+
+    fun nextStep() {
+        for (creature in creaturesMap.values.sortedWith(kotlin.Comparator({ t1, t2 -> t1.compareTo(t2) }))) {
+            if (creature.isAlive) {
+                creature.lifeStep()
+            }
+        }
+    }
+
+    private fun findFreePosition(): Pair<Int, Int> {
+        var randomPos: Int
+        var posCandidate: Pair<Int, Int>
+
+        do {
+            randomPos = (Math.random() * FIELD_SIZE_Y * FIELD_SIZE_X).toInt()
+            posCandidate = Pair(randomPos % FIELD_SIZE_X, randomPos / FIELD_SIZE_X)
+        } while (waterSpace[posCandidate.second][posCandidate.first] != FREE_WATER_CODE)
+
+        return posCandidate
+    }
+
     companion object {
+        private val TAG = "World"
         private val FIELD_SIZE_X = 10
         private val FIELD_SIZE_Y = 15
         private val ORCAS_PERCENT_FILLING = 5
         private val PENGUINS_PERCENT_FILLING = 100 - ORCAS_PERCENT_FILLING
-        private val CLEAR_WATER_CODE = -1
+        private val FREE_WATER_CODE = -1
     }
-
-    private var creaturesNumber: Map<Creature.Companion.Species, Int> = HashMap()
-    private var creaturesIdCounter = 0
-    private var waterSpace = array2dOfInt(FIELD_SIZE_Y, FIELD_SIZE_X)
-
 }
 
 fun array2dOfInt(sizeOuter: Int, sizeInner: Int): Array<IntArray>
